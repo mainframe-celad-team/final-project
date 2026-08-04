c4891022eed89ed5faaabb59533815fe6cdec251
Timdev0 <quentin.peltier.pro@gmail.com>
2026-08-04 16:49:20 +0200

    feat(dotnet): get cotation from api + class and mapping

---
+++ APIdevises/Cotation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APIdevises
+{
+    internal class Cotation
+    {
+        public required string  Code { get; init; }
+        public required decimal Value { get; init; }
+        public required DateOnly Date { get; init; }
+    }
+}
--- APIdevises/Program.cs
+++ APIdevises/Program.cs
@@ -1,7 +1,38 @@
-﻿internal class Program
+﻿using APIdevises;
+using Microsoft.Extensions.Configuration;
+
+internal class Program
 {
-    private static void Main(string[] args)
+    private static async Task Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        IConfiguration config = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+        string baseUrl = config["Frankfurter:BaseUrl"] 
+            ?? throw new InvalidOperationException("Configuration manquante : Frankfurter:BaseUrl");
+
+        var client = new FrankfurterClient(baseUrl);
+        List<RateDto> rates = await client.GetRatesAsync("USD");
+
+        Console.WriteLine($"Nombre de taux reçus : {rates.Count}");
+
+        foreach (var r in rates.Take(5))
+        {
+            Console.WriteLine($"{r.Quote} = {r.Rate} (base {r.Base}, {r.Date})");
+        }
+
+
+        var cotationMapper = new CotationMapper();
+        var cotations = cotationMapper.Map(rates);
+        Console.WriteLine($"Nombre de cotations mappées : {cotations.Count}");
+
+
+
+        foreach (var c in cotations.Take(5))
+        {
+            Console.WriteLine($"{c.Code} = {c.Value} (date : {c.Date})");
+        }
     }
 }
